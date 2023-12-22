2453a37d57d53fd10e543c7b565c98fbf0f2dec7
Fandresena Ravohary <hei.fandresena@gmail.com>
2023-12-22 09:21:51 +0300

    Feat: add BigSumController

---
+++ src/main/java/com/company/base/endpoint/rest/controller/health/BigSumController.java
@@ -0,0 +1,17 @@
+package com.company.base.endpoint.rest.controller.health;
+
+import java.math.BigInteger;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@AllArgsConstructor
+public class BigSumController {
+
+    @GetMapping("/big-sum")
+    public BigInteger add(@RequestParam BigInteger a, @RequestParam BigInteger b) {
+        return a.add(b);
+    }
+}
